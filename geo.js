155c080d614b3958d2b1dc03b2a2fe58c071e270
althafrahsan <althaf.rahsan@gmail.com>
2016-03-07 01:02:52 +0530

    Create geo.js

---
+++ geo.js
@@ -0,0 +1,9 @@
+<script language="Javascript">if (geoplugin_countryCode() != 'IN') {document.write('('+geoplugin_currencyConverter(800)+')');}</script>
+ 
+
+
+
+
+<script language="Javascript">if (geoplugin_countryCode() == 'IN') {document.write('('+geoplugin_currencyConverter(800)+')');}</script>
+ 
+
